a4587772ba69ae8756f1d0912a0d04660892096f
Liang, Yangxing <yangxing_liang@cathaypacific.com>
2017-07-17 15:20:15 +0800

    Initial Commit

---
+++ ReadHub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReadHub
+//
+//  Created by Liang, Yangxing on 17/7/2017.
+//  Copyright © 2017 Liang, Yangxing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
